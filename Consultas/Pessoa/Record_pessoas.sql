85388e3844331ce86871edc6634eb407eac56b7c
Ewerton Lima <32335520+ehzcl@users.noreply.github.com>
2019-10-08 23:54:53 -0300

    Update Record_pessoas.sql

--- Consultas/Pessoa/Record_pessoas.sql
+++ Consultas/Pessoa/Record_pessoas.sql
@@ -1,10 +1,10 @@
 DECLARE
-     info_pessoa pesssoa%ROWTYPE
+     info_pessoa pesssoa%ROWTYPE;
 BEGIN
-     info_pessoa.cpf_pessoa	 := '222.111.222-11'
-     info_pessoa.nome_pessoa	 := 'Juãozinho'
-     info_pessoa.data_nascimento := to_date('03/02/1998','dd/mm/yyyy')
-     info_pessoa.sexo_pessoa	 := 'H'
+     info_pessoa.cpf_pessoa	 := '222.111.222-11';
+     info_pessoa.nome_pessoa	 := 'Juãozinho';
+     info_pessoa.data_nascimento := to_date('03/02/1998','dd/mm/yyyy');
+     info_pessoa.sexo_pessoa	 := 'H';
 
-     INSERT INTO pessoa VALUES info_pessoa
+     INSERT INTO pessoa VALUES info_pessoa;
 END;
