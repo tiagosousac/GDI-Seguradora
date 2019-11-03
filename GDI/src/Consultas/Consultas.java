82ca569b9295d1dda8e8577f0b42f6e072976cae
Hugo <hsrf@cin.ufpe.br>
2019-11-02 23:43:21 -0300

    Consultas finalizadas

--- GDI/src/Consultas/Consultas.java
+++ GDI/src/Consultas/Consultas.java
@@ -1,15 +1,18 @@
 package Consultas;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
-
+import java.util.Vector;
 
 public class Consultas extends JFrame{
     public JTextField textField1;
     public JButton consultarButton;
     public JPanel consultas;
+    private JScrollPane scrollPane;
     public JTable table1;
     public Connection connection;
 
@@ -22,19 +25,9 @@ public class Consultas extends JFrame{
                 try {
                     String codigoConsulta = textField1.getText();
                     Statement consulta = connection.createStatement();
+
                     ResultSet result = consulta.executeQuery(codigoConsulta);
-                    ResultSetMetaData rsmd = result.getMetaData();
-                    System.out.println("?: "+ rsmd.getColumnName(1));
-                    System.out.println("?: "+ rsmd.getColumnName(2));
-                    System.out.println("?: "+ rsmd.getColumnName(3));
-                    System.out.println("?: "+ rsmd.getColumnName(4));
-                    while(result.next()){
-                        String nome = result.getString(1);
-                    }
-
-                    //consulta.close();
-                    //connection.close();
-
+                    table1.setModel(buildTableModel(result));
                 } catch (SQLException e2) {
                     JOptionPane.showMessageDialog(null, e2.getMessage());
                 }
@@ -42,4 +35,26 @@ public class Consultas extends JFrame{
         });
     }
 
+    public TableModel buildTableModel(ResultSet rs)
+            throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+        // names of columns
+        Vector<String> columnNames = new Vector<String>();
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+        return new DefaultTableModel(data, columnNames);
+    }
 }
+
+
